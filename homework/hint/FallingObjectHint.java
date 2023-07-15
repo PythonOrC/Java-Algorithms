84d2a6fe42aaf36949f44b517e4897bc2b15af07
Ethan Hu <ethanhu8351@gmail.com>
2023-07-15 16:52:20 -0700

    added homework/hint/FallingObjectHint.java

---
+++ homework/hint/FallingObjectHint.java
@@ -0,0 +1,14 @@
+// Don't at the answer now...
+// but here is a hint if you are truely stuck
+
+class FallingObjectHint {
+    public static void main(String[] args) {
+        double time = 10.0;
+        /*
+         * now try defining the other variables
+         * (position, g, initialPosition, initialVelocity)
+         * just like this one
+         */
+        System.out.println("The position of the object at " + time + " seconds is " + position + "m.");
+    }
+}
